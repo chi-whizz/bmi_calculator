7d7add8e96ca7f84aa9882125232856deb04c43f
chi-whizz <chibuike.techpundit@gmail.com>
2022-11-22 09:38:13 +0100

    Update the bmi_calc.c
    This commit added the neccessary conditional statements to reflect the
    approprate feedback messages of a BMI calculator. Before this commit,
    there was just a basic BMI mathematical layout which served as a bulding
    block base. I used relational and logical operators in the conditional
    statements because it will be easier to maintain.

--- bmi_calc.c
+++ bmi_calc.c
@@ -10,9 +10,19 @@ int main(void) {
 
 	float body_height_in_metres;
 	scanf("%f", &body_height_in_metres);
-
+	
 	float bmi = body_weight_in_kg / (body_height_in_metres * body_height_in_metres);
-	printf("Your BMI value is %.2f \n", bmi);
 
+	if (bmi < 18.5) {
+		printf("Your BMI value is %.2f \nIt's important to know that you are UNDERWEIGHT\n", bmi);
+	} else if (bmi >= 18.5 && bmi <= 24.9) {
+		printf("Your BMI value is %.2f \nIt's important to know that you have a HEALTHY weight\n", bmi);
+	} else if (bmi >= 25.0 && bmi <= 29.9) {
+		printf("Your BMI value is %.2f \nIt's important to know that you are OVERWEIGHT\n", bmi);
+	} else if (bmi >= 30.0) {
+		printf("Your BMI value is %.2f \nIts important to know that you are OBESE\n", bmi);
+	} else {
+		printf("None of the if-else-if ladder were executed");
+	}
 	return 0;
 }
